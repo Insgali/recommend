cee3185033be85af6f71ca3c97b83c4bd6400cb5
Insgali <93825755+Insgali@users.noreply.github.com>
2021-11-06 18:44:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,18 @@ import streamlit as st
 import pandas as pd
 
 st.header("Введите id пользователя:")
-id = st.number_input('', max_value=278, value=1)
+id = st.number_input(max_value=278, value=1)
 st.write('The current id is ', id)
 
 hist=pd.DataFrame({
-  'id':[11,22],
-  'title':['Построили школу', 'Открыли парк'],
-  'date':['10.09.2021', '12.09.2021']})
-st.header("History")
-st.write(hist)
+  'id':[11,22,23],
+  'title':['Построили школу', 'Открыли парк', 'Планируется открытие спорт центра'],
+  'date':['10.09.2021', '12.09.2021', '13.09.2021']})
 
 recomm=pd.DataFrame({
   'id':[110,202],
   'title':['Открыли центр развития детей', 'Открыли спортивный центр'],
   'date':['20.11.2021', '22.11.2021']})
-st.header("Recommendations")
-st.write(recomm)
 
 col1, col2 = st.columns(2)
 
