929569cf53440978cd92052f90ee23c08bcb9fce
Insgali <93825755+Insgali@users.noreply.github.com>
2021-11-06 17:29:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import os
 st.header("History")
 st.header("Recommendations")
 
-id_user = st.number_input('Insert a user id')
-st.write('The current id is ', number)
+id = st.number_input('Insert a user id')
+st.write('The current id is ', id)
